2b6fc87d5c87f8c4c7927be2ed178253b2657221
MP <markpaololibunao@gmail.com>
2022-10-14 16:35:14 +0800

    Moves prisma call to trpc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,16 @@
 import type { NextPage } from 'next'
-import { prisma } from '@/server/infra/db'
 import { trpc } from '@/utils/trpc'
 
 const Home: NextPage = (props: any) => {
-	const { data, isLoading } = trpc.useQuery(['hello'])
+	const { data, isLoading } = trpc.useQuery(['getAllQuestions'])
 
 	if (isLoading || !data) return <div>Loading...</div>
 
 	return (
 		<div>
-			<code>{props.questions}</code>
-			<div>{data?.greeting}</div>
+			<div>{data[0]?.question}</div>
 		</div>
 	)
 }
 
 export default Home
-
-export const getServerSideProps = async () => {
-	const questions = await prisma.pollQuestion.findMany()
-
-	return { props: { questions: JSON.stringify(questions) } }
-}
