8f4619043f2ee933a7569e3aff841704a9464a2f
MP <markpaololibunao@gmail.com>
2022-10-14 16:58:03 +0800

    Adds superjson as a data transformer for trpc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import { withTRPC } from '@trpc/next'
 import { AppType } from 'next/dist/shared/lib/utils'
 import type { AppRouter } from './api/trpc/[trpc]'
+import superjson from 'superjson'
 
 const MyApp: AppType = ({ Component, pageProps }) => {
 	return <Component {...pageProps} />
@@ -18,6 +19,7 @@ export default withTRPC<AppRouter>({
 			: 'http://localhost:3000/api/trpc'
 		return {
 			url,
+			transformer: superjson,
 			/**
 			 * @link https://react-query.tanstack.com/reference/QueryClient
 			 */
