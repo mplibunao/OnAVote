019bbd5a66f18d417bfb739c2a4a387c12e260c7
MP <markpaololibunao@gmail.com>
2022-10-16 14:44:56 +0800

    Creates base url helpers and updates trpc url to work on both csr and ssr on dev and on prod

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { withTRPC } from '@trpc/next'
 import { AppType } from 'next/dist/shared/lib/utils'
 import superjson from 'superjson'
 import type { AppRouter } from '@/backend/router'
+import { getBaseUrl } from '@/utils/trpc'
 
 const MyApp: AppType = ({ Component, pageProps }) => {
 	return <Component {...pageProps} />
@@ -11,7 +12,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
 export default withTRPC<AppRouter>({
 	config({ ctx }) {
 		return {
-			url: '/api/trpc',
+			url: `${getBaseUrl()}/api/trpc`,
 			transformer: superjson,
 			/**
 			 * @link https://react-query.tanstack.com/reference/QueryClient
--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,5 +1,13 @@
 import type { AppRouter } from '@/backend/router'
 import { createReactQueryHooks } from '@trpc/react'
+import { isServer } from './ssr'
 
 export const trpc = createReactQueryHooks<AppRouter>()
 // => { useQuery: ..., useMutation: ...}
+
+export function getBaseUrl() {
+	if (!isServer()) return '' // csr should use relative path
+	if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}` // ssr on vercel should use vercel url
+
+	return `http://localhost:${process.env.PORT ?? 3000}` // dev ssr should use localhost
+}
