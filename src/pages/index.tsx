62289ad708aa5c01aafbd4cc029746572c77ccf2
MP <markpaololibunao@gmail.com>
2022-10-17 16:28:52 +0800

    - Updates styling - Removes passHref props in next link since children is a direct anchor   tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,23 +9,30 @@ const Home: NextPage = () => {
 	if (isLoading || !data) return <div>Loading...</div>
 
 	return (
-		<div className='flex flex-col p-6'>
-			<div className='flex flex-col'>
+		<div className='flex w-screen flex-col items-stretch p-6'>
+			<div className='header flex w-full justify-between'>
 				<div className='text-2xl font-bold'>Your Questions</div>
+
+				<Link href='/create'>
+					<a className='rounded bg-gray-300 p-4 text-gray-800'>
+						Create New Question
+					</a>
+				</Link>
+			</div>
+
+			<div className='flex flex-col'>
 				{data.map((question) => (
-					<div className='my-2 flex flex-col' key={question.id}>
-						<Link href={`/question/${question.id}`} passHref>
-							<a>
-								<div className=''>{question.question}</div>
-							</a>
-						</Link>
-						<span>Created on {question.createdAt.toDateString()}</span>
-					</div>
+					<Link href={`/question/${question.id}`} key={question.id}>
+						<a>
+							<div className='my-2 flex flex-col'>
+								<div>{question.question}</div>
+
+								<span>Created on {question.createdAt.toDateString()}</span>
+							</div>
+						</a>
+					</Link>
 				))}
 			</div>
-			<Link href='/create' passHref>
-				<a>Create New Question</a>
-			</Link>
 		</div>
 	)
 }
