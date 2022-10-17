48579b2c1ff308aea1d18b8a789991a9f6e30293
MP <markpaololibunao@gmail.com>
2022-10-17 09:42:13 +0800

    Uses nanoid for generating cookie

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,4 @@
+import { nanoid } from 'nanoid'
 import { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
 
 export default async function middleware(
@@ -5,11 +6,11 @@ export default async function middleware(
 	_event: NextFetchEvent
 ): Promise<Response | undefined> {
 	const res = NextResponse.next()
-	console.log('req.cookies', req.cookies) // eslint-disable-line no-console
 
-	if (req.cookies.get('pollUserCookie')) return res
+	if (req.cookies.get('poll-token')) return res
 
-	const random = Math.random().toString()
-	res.cookies.set('pollUserCookie', random, { sameSite: 'strict' })
+	const random = nanoid()
+	res.cookies.set('poll-token', random, { sameSite: 'strict' })
+
 	return res
 }
