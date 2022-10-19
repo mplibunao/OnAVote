436ccdc34dd9eca680b81775013ecab204a1149b
MP <markpaololibunao@gmail.com>
2022-10-19 14:16:11 +0800

    Improves UI for the view question page

--- src/pages/question/[id].tsx
+++ src/pages/question/[id].tsx
@@ -1,58 +1,96 @@
+import { SEO } from '@/components/SEO'
 import { CreateQuestionValidator } from '@/shared/create-question-validator'
-import { trpc } from '@/utils/trpc'
+import { InferQueryOutput, trpc } from '@/utils/trpc'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 
 export interface QuestionsPageContentProps {
 	id: string
 }
 
+type Votes = InferQueryOutput<'questions.getById'>['votes']
+
 export const QuestionsPageContent = ({
 	id,
 }: QuestionsPageContentProps): JSX.Element => {
+	let totalVotes = 0
 	const { data, isLoading } = trpc.useQuery(['questions.getById', { id }])
 	const { mutate } = trpc.useMutation('questions.voteOnQuestion', {
-		onSuccess: () => window.location.reload(),
+		onSuccess: (response) => {
+			response.forEach(({ _count }) => (totalVotes += _count))
+			window.location.reload()
+		},
 	})
 
 	if (isLoading) return <div>Loading...</div>
 	if (!data) return <div>Question not found</div>
 
+	const getTotalVotes = (votes: Votes) =>
+		votes?.map(({ _count }) => (totalVotes += _count))
+
+	const getPercent = (voteCount?: number) => {
+		return voteCount && totalVotes > 0 ? (voteCount / totalVotes) * 100 : 0
+	}
+
+	if (data && data !== undefined) getTotalVotes(data.votes)
+
 	return (
-		<div className='flex flex-col p-8'>
-			{data?.isOwner && (
-				<div className='rounded-md bg-red-700 p-3'>You made this</div>
-			)}
+		<div className='container min-h-screen w-screen p-6'>
+			<SEO title='Question' description={data.question?.question} />
 
-			<div className='text-2xl font-bold'>{data?.question?.question}</div>
+			<header>
+				<Link href='/'>
+					<h1 className='cursor-pointer text-4xl font-bold'>OnAVote</h1>
+				</Link>
 
-			<div className='flex flex-col gap-4'>
-				{(data?.question?.options as CreateQuestionValidator['options'])?.map(
-					(option, index) => {
-						if (data?.isOwner || data?.vote) {
+				{data?.isOwner && (
+					<div className='rounded-md bg-gray-700 p-3'>You made this</div>
+				)}
+			</header>
+
+			<main className='mx-auto max-w-2xl'>
+				<div className='mb-10 text-center text-2xl font-bold'>
+					{data?.question?.question}
+				</div>
+
+				<div className='flex flex-col gap-4'>
+					{(data?.question?.options as CreateQuestionValidator['options'])?.map(
+						(option, index) => {
+							if (data?.isOwner || data?.vote) {
+								return (
+									<div key={index}>
+										<div className='flex justify-between'>
+											<p className='font-bold'>{option.text}</p>
+											<p>{getPercent(data.votes[index]?._count).toFixed()}</p>
+										</div>
+
+										<progress
+											className='progress progress-secondary w-full'
+											value={data.votes[index]?._count ?? 0}
+											max={totalVotes}
+										/>
+									</div>
+								)
+							}
+
 							return (
-								<div
+								<button
 									key={index}
-									className={data.vote?.choice === index ? 'underline' : ''}
-								>{`${data.votes[index]?._count ?? 0} - ${option.text}`}</div>
+									className='btn btn-outline'
+									onClick={() =>
+										mutate({
+											questionId: data?.question!.id,
+											option: index,
+										})
+									}
+								>
+									{option.text}
+								</button>
 							)
 						}
-
-						return (
-							<button
-								key={index}
-								onClick={() =>
-									mutate({
-										questionId: data?.question!.id,
-										option: index,
-									})
-								}
-							>
-								{option.text}
-							</button>
-						)
-					}
-				)}
-			</div>
+					)}
+				</div>
+			</main>
 		</div>
 	)
 }
